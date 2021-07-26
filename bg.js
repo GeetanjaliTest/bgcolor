d7e44f463c69cfdc208a7eb7485215e3d614afd1
GeetanjaliTest <giitanjalim@gmail.com>
2021-07-26 12:47:59 +0530

    Create bg.js

---
+++ bg.js
@@ -0,0 +1,40 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+   background(30);
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    box.position.x = box.position.x +5;
+    box.shapeColor= "pink";
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    box.position.x= box.position.x -5;
+    box.shapeColor="blue";
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    box.position.y = box.position.y - 5;
+    box.shapeColor="yellow";
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    box.position.y = box.position.y + 5;
+    box.shapeColor="orange";
+  }
+
+  drawSprites();
+}
